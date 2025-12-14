98d44a6d65bb92fed80f05258ab92ad34591bee7
b469 <69499965+b469@users.noreply.github.com>
2025-12-14 15:37:41 -0800

    Add files via upload
    Utilize the window.alert() and document.write() methods,
    Concatenate the variables together,
    Writ an expression,
    Write comments throughout you JavaScript explaining the code,
    Utilize the script element and src attribute to link to your main.js file

---
+++ JavaScript_file.js
@@ -0,0 +1,9 @@
+alert("Hello,World!") // JavaScript Alert Button
+
+
+
+function My_First_Function(){ //Defining a function and naming it
+    var str= "This is the button text!" //Defining a variable and giving it
+    document.getElementById("Botton_Text").innerHTML=str; //Putting the value of the variable
+                                                          //into the HTML element Form Point 
+}                                                         //with the "Button_Text" ID
