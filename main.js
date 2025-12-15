6598f4db33118290b99f83c2b7b32d9ca96d2b39
b469 <69499965+b469@users.noreply.github.com>
2025-12-15 12:12:12 -0800

    Add files via upload
    Basic JavaScript Projects'
    Project_2 function

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function displayDate() { //Adding Funtion
+  document.getElementById("Date").innerHTML = Date() //Adding id boxe for funtion
+}
+
+
+//JaveScript code for my fuction  += Operator"click Here"
+function myfunction() {
+var sentence = "I am Learning";
+sentence += " a lot from this book!";
+document.getElementById("concatenate").innerHTML = sentence;
+}
