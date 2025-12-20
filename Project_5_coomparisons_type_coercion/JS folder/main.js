6145b7a482c3312eb6e472086240a7bc38344946
b469 <69499965+b469@users.noreply.github.com>
2025-12-20 09:49:44 -0800

    Add files via upload
    Project_5_comparisons_type_coercion utilizing "the document.write and JS main files"and using operator :==,===,<,>, &&, ||, and display them in the browser

---
+++ Project_5_coomparisons_type_coercion/JS folder/main.js
@@ -0,0 +1,42 @@
+function my_Function(){ // Defining the name "NaN" False
+ document.getElementById("Test").innerHTML =  isNaN('007');
+
+}
+
+
+function my_NaN(){
+     document.getElementById("None").innerHTML = 0/0
+}
+
+
+
+
+
+
+function my_Tru(){ //Defining the name "NaN" True
+    document.getElementById("Positive").innerHTML = isNaN('This is a string');
+
+}
+
+
+// Boolean Logic False
+function False_number(){
+ document=(10<2);
+ document.getElementById("False").innerHTML = 10<2;
+}
+
+// Double Equal Sings ==
+function Double_sings(){
+    double = (3==10);
+    document.getElementById("Double").innerHTML = 3==10;
+}
+
+// The NOT Operator "!"
+function not_function(){ // Defining the name Not "!"" Operator "
+    document.getElementById("false").innerHTML = !(30>20);// response False
+}
+
+// The Not Operator "!" response True
+function true_function(){
+    document.getElementById("true").innerHTML = !(25>30);
+}
